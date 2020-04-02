41a45cc08a19ee331e1838b8eab1e8e718f26f83
timsterj <62201470+timsterj@users.noreply.github.com>
2020-04-02 15:49:28 +0300

    Initial commit

---
+++ app/src/main/java/com/timsterj/imgur/MainActivity.java
@@ -0,0 +1,14 @@
+package com.timsterj.imgur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
