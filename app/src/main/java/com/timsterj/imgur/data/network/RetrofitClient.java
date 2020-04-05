e902a18fb39014d35db846c85e3736bba9464cec
timsterj <62201470+timsterj@users.noreply.github.com>
2020-04-06 02:13:47 +0300

    19 Api error

--- app/src/main/java/com/timsterj/imgur/data/network/RetrofitClient.java
+++ app/src/main/java/com/timsterj/imgur/data/network/RetrofitClient.java
@@ -1,15 +1,7 @@
 package com.timsterj.imgur.data.network;
 
 
-import com.timsterj.imgur.contracts.Contracts;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
-
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -19,19 +11,19 @@ public class RetrofitClient {
 
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient(String baseURL){
+    public static Retrofit getClient(String baseURL) {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-//        OkHttpClient client = new OkHttpClient.Builder()
-//                .addInterceptor(interceptor)
-//                .build();
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
 
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseURL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//                    .client(client)
+                    .client(client)
                     .build();
         }
 
